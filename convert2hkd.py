b3dcda71d25a17963770989e27d9ab78cc38a286
Bitcarrot <73979971+bitkarrot@users.noreply.github.com>
2021-05-12 09:59:27 -0700

    update convert

--- convert2hkd.py
+++ convert2hkd.py
@@ -1,29 +1,44 @@
 import json
+import requests
 
 path = "./"
 
-try:
-    my_file = open(path + 'static/historical', 'rt')
-    lines = my_file.read()
-    my_file.close()
-    jlist = json.loads(lines)
-    
-    print(len(jlist))
+def convert():
+    try:
+        # grab file from site
+        r = requests.get("http://usdsat.com/historical")
+#        data = json.dumps(r)
+#        print(data)
+        with open(path + "static/historical", "wb") as f: 
+            f.write(r.content)
+        f.close()
 
-    for i in jlist:
-        price = i['satusd_rate']
-        i['sathkd_rate'] = int(price/7.75)
-        whole_price = i['btcusd_rate']
-        i['btchkd_rate'] = whole_price*7.75
+        
+        # convert to hkd
+        my_file = open(path + 'static/historical', 'rt')
+        lines = my_file.read()
+        print(lines)
+        my_file.close()
+        jlist = json.loads(lines)
+        
+        print(len(jlist))
+        
+        for i in jlist:
+            price = i['satusd_rate']
+            i['sathkd_rate'] = int(price/7.75)
+            whole_price = i['btcusd_rate']
+            i['btchkd_rate'] = whole_price*7.75
 
-    print(jlist[len(jlist)-1])
+        print(jlist[len(jlist)-1])
 
-    with open(path + 'static/hkd_historical', 'w') as output:
-        output.write(json.dumps(jlist))
-    output.close()
-    
-except Exception as e:
-    print(e)
-    print("Something unexpected occurred!")
+        with open(path + 'static/hkd_historical', 'w') as output:
+            output.write(json.dumps(jlist))
+        output.close()
+       
+    except Exception as e:
+        print(e)
+        print("Something unexpected occurred!")
 
 
+
+convert()
